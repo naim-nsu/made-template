b058588beebdd990a44eed673c5b2189af719066
AD005\z004v62z <mohammad.naim@siemens-healthineers.com>
2023-12-20 17:56:13 +0100

    modify config.example

--- project/config/config_var.example.py
+++ project/config/config_var.example.py
@@ -5,3 +5,5 @@ SOURCE_INFO_PATH = os.path.join(BASE_DIR, "project", "config", "source_info.json
 DB_PATH = os.path.join(BASE_DIR, "project", "data", "fau_made_project_ws23.sqlite")
 GENESIS_USERNAME= os.environ['GENESIS_USERNAME']
 GENESIS_PASSWORD= os.environ['GENESIS_PASSWORD']
+
+print(os.environ)
