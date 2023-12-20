a131561c84cabb7233715abdd725ea173a114f8a
AD005\z004v62z <mohammad.naim@siemens-healthineers.com>
2023-12-20 16:49:27 +0100

    update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
+
+
+# Path to the directory where you want to check for the file
+directory_path="/project/config"
+
+# Name of the file you're checking for
+file_to_check="config_var.py"
+
+# Name of the file to duplicate
+file_to_duplicate="config_var.example.py"
+
+
+# Check if the file exists in the specified directory
+if [ -f "${directory_path}/${file_to_check}" ]; then
+    echo "File '${file_to_check}' exists."
+else
+    if [ -f "${directory_path}/${file_to_duplicate}" ]; then
+        # Duplicate the file if it exists and rename it
+        cp "${directory_path}/${file_to_duplicate}" "${directory_path}/${file_to_check}"
+        echo "File '${file_to_check}' created by duplicating '${file_to_duplicate}'."
+    else
+        echo "File '${file_to_duplicate}' does not exist to duplicate."
+    fi
+fi
+
 python ./project/tests.py
